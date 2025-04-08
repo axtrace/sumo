ba011e3685369040b323068928dd7a0ccb8e7b09
axtrace <4077607+axtrace@users.noreply.github.com>
2025-04-08 17:51:42 +0300

    add list import

--- ydb_adapter.py
+++ ydb_adapter.py
@@ -4,7 +4,7 @@ import ydb.iam
 import json
 import uuid
 from datetime import datetime
-from typing import Dict, Any
+from typing import Dict, Any, List
 from telebot import types
 
 class YdbAdapter:
